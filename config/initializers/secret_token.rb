78a9df3017ecdc4dbbaeb30fca310b1cf50f3dbc
Enrique <quiq64@quiq64-M720-US3.(none)>
2012-10-14 12:44:55 -0500

    Primera Versión

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitrails::Application.config.secret_token = '4e9c49147f1a687d26317b37f7187c9a70f4b9ee0fb031dd22261af8654db02e7990c2341958ff8bb1eb22132a944a62db8d08e1055c3f8e9ddf553890691f7c'
